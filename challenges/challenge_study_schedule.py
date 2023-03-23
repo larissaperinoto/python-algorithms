b1dc481dad27bb3fd99ccdab32027e70b6554a94
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-23 17:41:22 +0100

    feat: implementa função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    counter = 0
+
+    try:
+        for start, stop in permanence_period:
+            if start <= target_time <= stop:
+                counter += 1
+
+        return counter
+
+    except(TypeError):
+        return None
+
