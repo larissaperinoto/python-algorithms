d750cda27ad592de1d1c522d73bb6c9930252d04
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-23 21:57:22 +0100

    feat: insere função is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if word == "".join(reversed(word)):
+        return True
+
+    return False
