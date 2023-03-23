fa3c489fb8f7138645f544950eb0c4970d27c474
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-23 20:50:47 +0100

    tests: insere testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(key='A', message='abc')
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(key=1, message=2)
+
+    result1 = encrypt_message(key=100, message='abc')
+    odd_key = encrypt_message(key=1, message='abc')
+    even_key = encrypt_message(key=2, message='abc')
+
+    assert result1 == 'cba'
+    assert odd_key == 'a_cb'
+    assert even_key == 'c_ba'
+
