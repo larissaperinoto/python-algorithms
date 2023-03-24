0f61783a1562edf34d5662c3e3fd9d0e42fee9d5
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-24 10:22:17 +0100

    fiz: corrige linter

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -8,7 +8,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[low_index] == word[high_index]:
         low_index += 1
         high_index -= 1
-        return is_palindrome_recursive(word, low_index=low_index, high_index=high_index)
+        return is_palindrome_recursive(word, low_index, high_index)
 
     else:
         return False
