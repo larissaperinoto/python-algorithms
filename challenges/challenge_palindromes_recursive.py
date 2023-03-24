4daf086e11374caf23d9c77d3d079c8808e470d0
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-24 10:17:58 +0100

    feat: insere função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index=(low_index + 1), high_index=(high_index - 1))
+
+    else:
+        return False
