0047c60503c3e52af1bff3e7b5f8142fa3a2b58a
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-24 15:43:11 +0100

    fix: corrige complexidade da função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,14 +1,29 @@
 def find_duplicate(nums):
 
-    new = []
+    a = {}
+
     try:
         for num in nums:
-            if int(num) < 0:
-                raise TypeError
-            if num not in new:
-                new.append(num)
-            else:
-               return num
+            a.update(create_dict(a, num))
+
+            if a[num] > 1:
+                return num
+
         return False
     except (TypeError, ValueError):
         return False
+
+
+def create_dict(a, num):
+
+    if int(num) < 0:
+        raise TypeError
+
+    try:
+        a[num] = a[num] + 1
+
+    except (KeyError):
+
+        a[num] = 1
+
+    return a
