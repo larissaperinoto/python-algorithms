f19fdf14c3c64b53ecd5d2f18c83ec0917bef8d8
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-26 09:29:13 +0200

    fix: corrige complexidade da função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,27 +1,46 @@
 def is_anagram(first_string, second_string):
+
     if len(first_string) == 0 and len(second_string) == 0:
         return (first_string, second_string, False)
 
-    first = selection_sort(list(first_string.lower()))
-    second = selection_sort(list(second_string.lower()))
-    f_ordered = ''.join(first)
-    s_ordered = ''.join(second)
+    a = list(first_string.lower())
+    b = list(second_string.lower())
 
-    return (f_ordered, s_ordered, f_ordered == s_ordered)
+    merge_sort(a)
+    merge_sort(b)
+
+    first = ''.join(a)
+    second = ''.join(b)
+
+    return (first, second, first == second)
 
 
-def selection_sort(string):
-    n = len(string)
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)
 
-    for index in range(n - 1):
-        min_element_index = index
 
-        for search_index in range(index + 1, n):
-            if string[search_index] < string[min_element_index]:
-                min_element_index = search_index
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]
+    right = numbers[mid:end]
 
-        current_element = string[index]
-        string[index] = string[min_element_index]
-        string[min_element_index] = current_element
+    left_index, right_index = 0, 0
 
-    return string
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
